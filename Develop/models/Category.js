4515c0018034306dc1b0339370a6fae7fa612498
Patrick Morgan <patrickdmorgan2@gmail.com>
2023-07-24 13:21:37 -0400

    added starter code

---
+++ Develop/models/Category.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataType.Integer,
+      allowNull: false,
+      
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
