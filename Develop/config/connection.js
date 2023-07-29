1669f1bdba769c1865344d2a5fdee0ac0bc68983
Patrick Morgan <patrickdmorgan2@gmail.com>
2023-07-29 18:38:56 -0400

    fixed connection hard coded in connection.js

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,10 +2,8 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+const sequelize = new Sequelize('ecommerce_db', 'root', 'password', {
+      host: '127.0.0.1',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
